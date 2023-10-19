c139a8c7a4b7445292a5798e5e7cd01d184de492
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-10-20 02:16:56 +0600

    Implemented the UpdateProduct page: - Fetches the product data to be updated using the ID from the URL. - Displays a loading indicator while fetching data. - After data is loaded, it shows the product update form using the 'ProductForm' component. - The 'ProductForm' component is used to update the product with new data. - Handles the update of a product with a PATCH request to the server. - Provides user feedback with success and error toasts.

---
+++ src/pages/product/UpdateProduct.jsx
@@ -0,0 +1,77 @@
+import { useParams } from "react-router-dom";
+import PageHeader from "../../components/PageHeader";
+import ProductForm from "./ProductForm";
+import { useEffect, useState } from "react";
+import Loader from "../../components/Loader";
+import toast from "react-hot-toast";
+
+// const {
+//   name,
+//   imageUrl,
+//   types,
+//   brandName,
+//   price,
+//   description,
+//   rating,
+//   new: ProductNew,
+//   sale,
+// } = useLoaderData();
+const UpdateProduct = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log(data);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_SERVER_URL}/product/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleUpdate = async (value) => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_URL}/product/${id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(value),
+        }
+      );
+
+      if (!response.ok) {
+        toast.error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      toast.success("Product Update Successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+  return (
+    <div className="bg-[#f6eeff]">
+      <PageHeader title="Create Product" currentPage="Product add" />
+      <div className=" container mx-auto px-5">
+        <ProductForm getValue={handleUpdate} initialData={data} />
+      </div>
+    </div>
+  );
+};
+
+export default UpdateProduct;
