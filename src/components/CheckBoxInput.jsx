392432ef5bfdb2f3cc91454237df8a02f9a5989d
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-10-19 19:23:53 +0600

    Added CheckBoxInput component: - Created a new component for rendering checkboxes with labels. - Improved the code for better readability and maintainability.

---
+++ src/components/CheckBoxInput.jsx
@@ -0,0 +1,20 @@
+const CheckBoxInput = ({ label, ...argument }) => {
+  return (
+    <div className="flex items-center mb-4">
+      <input
+        {...argument}
+        id={label}
+        type="checkbox"
+        value=""
+        className="w-6 h-6 cursor-pointer text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+      />
+      <label
+        htmlFor={label}
+        className="ml-2 text-md font-medium text-gray-900 ">
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default CheckBoxInput;
